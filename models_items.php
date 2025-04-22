4196157d125d7adb251b13b07b9e9314093d16d2
Jaild Katte C. Lapis <158005981+jaildkatte@users.noreply.github.com>
2025-04-22 23:25:53 +0800

    Create models_items.php

---
+++ models_items.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'Book' => [
+        'desc' => 'A fantasy novel with beautiful illustrations.',
+        'img' => 'none'
+    ],
+    'Hat' => [
+        'desc' => 'A stylish hat, barely used.',
+        'img' => 'none'
+    ],
+    'Backpack' => [
+        'desc' => 'Spacious backpack for daily use.',
+        'img' => 'none'
+    ],
+    'Shoes' => [
+        'desc' => 'Lightweight running shoes.',
+        'img' => 'none'
+    ],
+];
